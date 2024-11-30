6329c9ff4b837f0b6c95d86eb513842790294a13
jenilkukadiya <jenilkukadiya82@gmail.com>
2024-12-01 00:23:07 +0530

    Initial commit

---
+++ src/test/java/com/jenil/LibraryManagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenil.LibraryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
